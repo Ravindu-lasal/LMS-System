1bb4c1c9439fef3aa19cdff568b2d23617e914c1
Ravindu lasal <ravindusl01@gmail.com>
2024-11-16 10:47:23 +0530

    update and link admin pages

--- learning-mangement-system/pages/admin/index.php
+++ learning-mangement-system/pages/admin/index.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta name="description" content="" />
   <meta name="author" content="" />
-  <title>Dashboard - SLTB Admin</title>
+  <title>Dashboard - LMS Admin</title>
 
   <!-- stylesheet table and site -->
   <link href="css/styles.css" rel="stylesheet" />
@@ -55,8 +55,8 @@
       <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
           <div class="nav">
-            <div class="sb-sidenav-menu-heading">Core</div>
-            <a class="nav-link" href="index.html">
+            <div class="sb-sidenav-menu-heading">Main</div>
+            <a class="nav-link" href="index.php">
               <div class="sb-nav-link-icon">
                 <i class="fas fa-tachometer-alt"></i>
               </div>
@@ -68,7 +68,7 @@
               <div class="sb-nav-link-icon">
                 <i class="fas fa-columns"></i>
               </div>
-              Layouts
+              Manage 
               <div class="sb-sidenav-collapse-arrow">
                 <i class="fas fa-angle-down"></i>
               </div>
@@ -77,69 +77,39 @@
               <nav class="sb-sidenav-menu-nested nav">
                 <a class="nav-link" href="users.php">Manage Users</a>
                 <a class="nav-link" href="subject.php">Manage Subject</a>
-                <a class="nav-link" href="./student_feedback.php">Students Feedback</a>
               </nav>
             </div>
             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages"
-              aria-expanded="false" aria-controls="collapsePages">
+              aria-expanded="false" aria-controls="collapseLayouts">
               <div class="sb-nav-link-icon">
                 <i class="fas fa-book-open"></i>
               </div>
-              Pages
+              Interface
               <div class="sb-sidenav-collapse-arrow">
                 <i class="fas fa-angle-down"></i>
               </div>
             </a>
-            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-              <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth"
-                  aria-expanded="false" aria-controls="pagesCollapseAuth">
-                  Authentication
-                  <div class="sb-sidenav-collapse-arrow">
-                    <i class="fas fa-angle-down"></i>
-                  </div>
-                </a>
-                <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
-                  data-bs-parent="#sidenavAccordionPages">
-                  <nav class="sb-sidenav-menu-nested nav">
-                    <a class="nav-link" href="login.html">Login</a>
-                    <a class="nav-link" href="register.html">Register</a>
-                    <a class="nav-link" href="password.html">Forgot Password</a>
-                  </nav>
-                </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError"
-                  aria-expanded="false" aria-controls="pagesCollapseError">
-                  Error
-                  <div class="sb-sidenav-collapse-arrow">
-                    <i class="fas fa-angle-down"></i>
-                  </div>
-                </a>
-                <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
-                  data-bs-parent="#sidenavAccordionPages">
-                  <nav class="sb-sidenav-menu-nested nav">
-                    <a class="nav-link" href="401.html">401 Page</a>
-                    <a class="nav-link" href="404.html">404 Page</a>
-                    <a class="nav-link" href="500.html">500 Page</a>
-                  </nav>
-                </div>
+            <div class="collapse" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+              <nav class="sb-sidenav-menu-nested nav">
+                <a class="nav-link" href="../../index.php">Main Page</a>
+                <a class="nav-link" href="../signup.php">Sign in </a>
+                <a class="nav-link" href="subject.php">Sign up </a>
               </nav>
             </div>
-            <div class="sb-sidenav-menu-heading">Addons</div>
-            <a class="nav-link" href="charts.html">
+
+
+            <div class="sb-sidenav-menu-heading">User review</div>
+            <a class="nav-link" href="./student_feedback.php">
               <div class="sb-nav-link-icon">
                 <i class="fas fa-chart-area"></i>
               </div>
-              Charts
-            </a>
-            <a class="nav-link" href="tables.html">
-              <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-              Tables
+              Students Feedback
             </a>
           </div>
         </div>
         <div class="sb-sidenav-footer">
           <div class="small">Logged in as:</div>
-          SLTB Mawanelle Depot
+          Lecture Managment System
         </div>
       </nav>
     </div>
