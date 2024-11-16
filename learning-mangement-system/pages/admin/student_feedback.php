df29a4ca302a8af0abf94e6bdf0131810fcadc21
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-17 02:36:27 +0530

    Fixed Admin Page nagiations and page links

--- learning-mangement-system/pages/admin/student_feedback.php
+++ learning-mangement-system/pages/admin/student_feedback.php
@@ -90,24 +90,20 @@ if (isset($_POST['delete']) && isset($_POST['f_id'])) {
         <!-- Navbar Brand-->
         <a class="navbar-brand ps-3" href="index.html">ADIMIN PANEL</a>
         <!-- Sidebar Toggle-->
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
+            <i class="fas fa-bars"></i>
+        </button>
         <!-- Navbar Search-->
         <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
             <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
             </div>
         </form>
         <!-- Navbar-->
         <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Settings</a></li>
-                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
                     <li><a class="dropdown-item" href="../../inc/logout.php">Logout</a></li>
                 </ul>
             </li>
@@ -147,32 +143,19 @@ if (isset($_POST['delete']) && isset($_POST['f_id'])) {
                             <div class="sb-nav-link-icon">
                                 <i class="fas fa-book-open"></i>
                             </div>
-                            Interfaces
-                            <div class="sb-sidenav-collapse-arrow">
-                                <i class="fas fa-angle-down"></i>
-                            </div>
-                        </a>
-                        <div class="collapse" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                            <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="../../index.php">Main Page</a>
-                                <a class="nav-link" href="../signup.php">Sign in </a>
-                                <a class="nav-link" href="../signup.php">Sign up </a>
-                            </nav>
-                        </div>
 
-
-                        <div class="sb-sidenav-menu-heading">User review</div>
-                        <a class="nav-link" href="./student_feedback.php">
-                            <div class="sb-nav-link-icon">
-                                <i class="fas fa-chart-area"></i>
-                            </div>
-                            Students Feedback
-                        </a>
+                            <div class="sb-sidenav-menu-heading">User review</div>
+                            <a class="nav-link" href="./student_feedback.php">
+                                <div class="sb-nav-link-icon">
+                                    <i class="fas fa-chart-area"></i>
+                                </div>
+                                Students Feedback
+                            </a>
                     </div>
                 </div>
                 <div class="sb-sidenav-footer">
                     <div class="small">Logged in as:</div>
-                    Lecture Managment System
+                    <?php echo $_SESSION['admin_name'] ?>
                 </div>
             </nav>
         </div>
