28d48f566980a4e14b724cc545b55a7d5755a31e
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-17 00:59:32 +0530

    Fixed a File Path direction

--- learning-mangement-system/pages/student_inc/stu_nav.php
+++ learning-mangement-system/pages/student_inc/stu_nav.php
@@ -6,7 +6,7 @@
         </div>
         <ul class="nav-links">
             <li><a href="./stu_dashboard.php">Home</a></li>
-            <li><a href="./courses_without_login.php">Courses</a></li>
+            <li><a href="./stu_course.php">Courses</a></li>
             <li><a href="./stu_about.php">About</a></li>
             <li><a href="./feedback.php">Contact</a></li>
         </ul>
